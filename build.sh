016bfdc71bb6ecdad4cc214e160db145b90d9588
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-03-16 19:27:42 +0100

    🔧 build: generate pdf

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 pandoc --standalone \
-  --metadata pagetitle="Asymptotic Stability for Humans" \
   --css css/style.css \
   --include-in-header html/font.html \
   -V lang=en \
-  -f gfm \
   -o doc.html doc.md
+
+pandoc --standalone \
+  -V lang=en \
+  -o doc.pdf doc.md
